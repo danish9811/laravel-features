0cd884751c3f8390352cbffb5b1527bec1b66bca
Danish Mehmood <danishmehmood981@gmail.com>
2023-01-04 17:19:09 +0500

    Added a new controller to create and show new hashes

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use JetBrains\PhpStorm\NoReturn;
+
+class TestController extends Controller {
+    /**
+     * @throws Exception
+     */
+    #[NoReturn] public function showHashes() {
+        dd($this->createHashes());
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function createHashes() {
+        return [
+            Hash::make(random_int(10, 20)),
+            Hash::make(random_int(10, 20)),
+            Hash::make(random_int(10, 20)),
+            Hash::make(random_int(10, 20)),
+            Hash::make(random_int(10, 20)),
+        ];
+    }
+
+}
