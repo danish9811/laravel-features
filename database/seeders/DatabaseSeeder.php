32bbdd44543f53d07c10c9e39df8f7baaa523625
Danish Mehmood <danishmehmood981@gmail.com>
2023-01-04 17:07:31 +0500

    Import fixes for models in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\{Book, Employee, User};
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        \App\Models\User::factory(10)->create();
-        \App\Models\Book::factory(10)->create();
-        \App\Models\Employee::factory(10)->create();
+class DatabaseSeeder extends Seeder {
+
+    public function run() {
+        User::factory(10)->create();
+        Book::factory(100)->create();
+        Employee::factory(10)->create();
     }
 }
