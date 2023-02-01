0dd38e2a10bcea0a71557f05e5421559141f8f9f
Danish Mehmood <danishmehmood981@gmail.com>
2023-02-01 13:11:57 +0500

    routes are now group routes

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,20 @@ Route::get('/', static function () {
     return phpinfo();
 });
 
-Route::get('/login', [PassportAuthController::class, 'showLoginForm'])->name('login');
-Route::get('/register', [PassportAuthController::class, 'showRegisterForm'])->name('register');
-Route::get('/login-submit', [PassportAuthController::class, 'passportAuthLoginSubmit']);
-Route::get('/register-submit', [PassportAuthController::class, 'passportAuthRegisterSubmit']);
-Route::get('/chart', [PassportAuthController::class, 'showApexChart'])->middleware('auth')->name('apex-chart');
+Route::controller(PassportAuthController::class)->group(static function () {
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::get('/register', 'showRegisterForm')->name('register');
+    Route::post('/login-submit', 'passportAuthLoginSubmit');
+    Route::post('/register-submit', 'passportAuthRegisterSubmit');
+    Route::get('/chart', 'showApexChart')->middleware('auth')->name('apex-chart');
 
-Route::get('/search', [SearchController::class, 'index'])->name('search');
-Route::get('/autocomplete', [SearchController::class, 'autocomplete'])->name('autocomplete');
+});
+
+Route::controller(SearchController::class)->group(static function () {
+    Route::get('/search', 'index')->name('search');
+    Route::get('/autocomplete', 'autocomplete')->name('autocomplete');
+});
+
 Route::get('/pen', PenController::class);
 
 Route::get('/query-test', [TestController::class, 'testHotelBookingQuery']);
