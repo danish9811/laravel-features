96241e70952a73e2ff1ab664ec7dad7138e6a4a4
Danish Mehmood <danishmehmood981@gmail.com>
2023-02-04 22:07:27 +0500

    added few spaces

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,13 +8,18 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder {
 
     public function run() {
+
         User::truncate();
         User::factory(20)->create();
+
         Book::truncate();
         Book::factory(200)->create();
+
         Employee::truncate();
         Employee::factory(50)->create();
+
         Item::truncate();
         Item::factory(100)->create();
+
     }
 }
