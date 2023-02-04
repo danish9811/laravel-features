b0facba66b5dcdc4ea8a70079377075f9a4a6a25
Danish Mehmood <danishmehmood981@gmail.com>
2023-02-04 18:35:58 +0500

    added factory for Item model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,14 +2,19 @@
 
 namespace Database\Seeders;
 
-use App\Models\{Book, Employee, User};
+use App\Models\{Book, Employee, Item, User};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder {
 
     public function run() {
+        User::truncate();
         User::factory(20)->create();
+        Book::truncate();
         Book::factory(200)->create();
+        Employee::truncate();
         Employee::factory(50)->create();
+        Item::truncate();
+        Item::factory(100)->create();
     }
 }
