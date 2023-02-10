268f5e52007c769d29021c843168df9f697b50da
Danish Mehmood <danishmehmood981@gmail.com>
2023-02-10 18:40:06 +0500

    Web routes updated

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,26 @@ Route::get('/', static function () {
     return phpinfo();
 });
 
+// postman login get and push
 Route::get('/login', [PassportAuthController::class, 'showLoginForm'])->name('login');
 Route::get('/register', [PassportAuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/login-submit', [PassportAuthController::class, 'passportAuthLoginSubmit']);
 Route::get('/register-submit', [PassportAuthController::class, 'passportAuthRegisterSubmit']);
 
+// how chart is rendered
 Route::get('/chart', [PassportAuthController::class, 'showApexChart'])->middleware('auth')->name('apex-chart');
 
-Route::controller(SearchController::class)->group(static function () {
-    Route::get('/search', 'index')->name('search');
-    Route::get('/autocomplete', 'autocomplete')->name('autocomplete');
-});
 
+// implementing search feature
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+Route::get('/autocomplete', 'autocomplete')->name('autocomplete');
+
+// single action controller
 Route::get('/pen', PenController::class);
 
+// learning how to optimize queries
 Route::get('/query-test', [TestController::class, 'testHotelBookingQuery']);
 
-Route::controller(StripePaymentController::class)->group(function () {
-    Route::get('stripe', 'stripe');
-    Route::post('stripe', 'stripePost')->name('stripe.post');
-});
-
+// implementing stripe
+Route::get('/stripe', [StripePaymentController::class, 'stripe']);
+Route::post('/stripe', [StripePaymentController::class, 'stripePost'])->name('stripe.post');
